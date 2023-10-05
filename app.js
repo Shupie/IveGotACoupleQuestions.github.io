6ecc43ae1062519f6998a568a7f661972d8211cc
Shupie <seth.shupak@gmail.com>
2023-10-05 15:05:42 -0500

    dag

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
-// Get the counter element
-const counter = document.querySelector("#counter");
+// Get the text area element
+const textArea = document.querySelector("#text-area");
 
-// Initialize the click count
-let clickCount = 0;
+// Create an array of random words
+const words = ["Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit", "sed", "do", "eiusmod", "tempor", "incididunt", "ut", "labore", "et", "dolore", "magna", "aliqua"];
 
-// Update the counter display when the user clicks
+// Create a flag to indicate whether the text generator is running
+let isRunning = false;
+
+// Start the text generator when the user clicks on the screen
 document.addEventListener("click", function() {
-  clickCount++;
-  counter.textContent = clickCount;
+  isRunning = true;
+  generateText();
 });
 
-// Save the click count to local storage
-localStorage.setItem("clickCount", clickCount);
-
-// Restore the click count from local storage when the page loads
-window.addEventListener("load", function() {
-  clickCount = localStorage.getItem("clickCount");
-  counter.textContent = clickCount;
+// Stop the text generator when the user clicks on the screen again
+document.addEventListener("click", function() {
+  isRunning = false;
 });
+
+// Generate random text
+function generateText() {
+  // Get a random word from the array
+  const word = words[Math.floor(Math.random() * words.length)];
+
+  // Add the word to the text area
+  textArea.textContent += word + " ";
+
+  // If the text generator is still running, request the next animation frame
+  if (isRunning) {
+    requestAnimationFrame(generateText);
+  }
+}
+
+// Start the text generator when the page loads
+window.addEventListener("load", generateText);
